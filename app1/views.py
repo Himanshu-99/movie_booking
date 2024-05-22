1d22f1922e07bb6eab9d5dbae382242ba8fced5c
Himanshu <hanwatehimanshu99@gmail.com>
2024-05-22 21:04:11 +0530

    changes done

--- app1/views.py
+++ app1/views.py
@@ -1,38 +1,52 @@
 import json
 from django.shortcuts import render
-import requests  # Import requests library
+import requests
+
 
 def get_movie_data_url():
     return 'http://127.0.0.1:3000/movies/'
 
+
 def movie_list(request):
     movies = []
     selected_genre = request.GET.get('genre')
+    search_term = request.GET.get('search_query', '')
+
     try:
         url = get_movie_data_url()
         response = requests.get(url)
         if response.status_code == 200:
             data = json.loads(response.content)
-            for movie_data in data:
-                movies.append({
-                    'title': movie_data['movies'][0]['title'],
-                    'poster': movie_data['movies'][0]['poster'],
-                    'genre': ', '.join(movie_data['movies'][0]['genre']),  # Join genres as a string
-                    'rating': movie_data['movies'][0]['Ratings'][0]['value'] if movie_data['movies'][0]['Ratings'] else 'N/A',
-                    'year_release': int(movie_data['movies'][0]['released'].split(' ')[0]),  # Extract year from released date
-                    'metacritic_rating': movie_data['movies'][0]['meta_score'] if movie_data['movies'][0]['meta_score'] else 'N/A',
-                    'runtime': movie_data['movies'][0]['runtime'],
-                })
+
+
+            if isinstance(data, list):
+
+                for item in data:
+                    movies.extend(item.get("movies", []))
+            elif isinstance(data, dict):
+
+                movies = data.get("movies", [])
+            else:
+                print(f"Warning: Unexpected data structure received from API.")
+
     except Exception as e:
-        print(f'Error fetching data: {e}')  # Log or handle error appropriately
+        print(f'Error fetching data: {e}')
 
     if selected_genre:
-        movies = [movie for movie in movies if selected_genre in movie['genre']]
+        movies = [movie for movie in movies if any(genre == selected_genre for genre in movie['genre'].split(','))]
 
-    genres = list(set([genre for movie in movies for genre in movie['genre'].split(',')]))
+    if search_term:
+        movies = [movie for movie in movies if search_term in movie['title']]
+
+    genres = []
+    for movie in movies:
+        if 'genre' in movie and isinstance(movie['genre'], str):
+            genres.extend(genre.strip() for genre in movie['genre'].split(','))
+    genres = list(set(genres))
     context = {
         'movies': movies,
         'genres': genres,
         'selected_genre': selected_genre,
+        'search_term': search_term,
     }
     return render(request, 'index.html', context)
